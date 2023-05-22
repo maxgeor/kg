b6d5d61179517b0155e0434e4aee533c94734558
Max <73665502+maxgeor@users.noreply.github.com>
2023-05-22 16:56:56 -0400

    contact page glow up

--- web/app/contact/page.tsx
+++ web/app/contact/page.tsx
@@ -4,16 +4,6 @@ import Link from "next/link";
 export default function Contact() {
   return (
     <>
-      <div className="col-span-full md:col-span-1 flex flex-col gap-y-3">
-        <Link
-          href="https://www.instagram.com/kg.handcrafted/"
-          target="_blank"
-          className="underline -my-1"
-        >
-          Instagram
-        </Link>
-        <p className="-my-1">kevin_g8405@hotmail.com</p>
-      </div>
       <div className="col-span-full md:col-span-3">
         <p className="-my-1">
           To buy a knife or connect,{" "}
@@ -26,6 +16,28 @@ export default function Contact() {
           </Link>{" "}
           or send an email.
         </p>
+      </div>
+      <div className="md:order-first col-span-full md:col-span-1 flex flex-col gap-y-3">
+        <table cellPadding={0} cellSpacing={0}>
+          <tbody>
+            <tr>
+              <td className="w-3 pr-3 ">i:</td>
+              <td>
+                <Link
+                  href="https://www.instagram.com/kg.handcrafted/"
+                  target="_blank"
+                  className="underline -my-1"
+                >
+                  @kg.handcrafted
+                </Link>
+              </td>
+            </tr>
+            <tr>
+              <td className="w-3 pr-3 ">e:</td>
+              <td>kevin_g8405@hotmail.com</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </>
   );
